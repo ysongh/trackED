5aa8321289302784f181bfc3c1d37020bfe6e83c
You Song Hou <yousonghou@yahoo.com>
2019-08-30 23:07:51 -0400

    install victory and add charts

---
+++ src/components/common/LineChart.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { VictoryBar ,VictoryLine, VictoryChart,VictoryTooltip} from 'victory';
+
+const LineChart = props => {
+		return (
+			<div>
+				<div className="chart" style={{width:"60%"}}>
+				<VictoryChart
+					domainPadding={30}
+				>
+					{props.chartType === "LINE" ?
+						<VictoryLine
+							labelComponent={<VictoryTooltip/>}
+							data={
+								props.grades.map((grades,index) => ({type: grades.type, grade: grades.grade}))
+							}
+							domain={{y: [0, 100]}}
+							x="type"
+							y="grade"
+							style={{
+								labels: {fontSize: 12},
+								parent: {border: "4px solid #ccc"},
+								width: "60%"
+							}}
+						/>
+						:
+						<VictoryBar
+							data={
+								props.grades.map(grades => ({type: grades.type, grade: grades.grade}))
+							}
+							domain={{y: [0, 100]}}
+							x="type"
+							y="grade"
+							style={{
+								data: {fill: (d) => d.y > 60 ? "#1ABC9C" : "#DD1379"},
+								labels: {fontSize: 12},
+								parent: {border: "4px solid #ccc"},
+								width: "60%"
+							}}
+						/>
+					}
+				</VictoryChart>
+			    </div>
+			</div>
+		);
+	};
+
+export default LineChart;
--- src/components/student/Student.js
+++ src/components/student/Student.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 
 import defaultUserImage from '../../img/defaultUser.png';
+import LineChart from '../common/LineChart';
 
 class Student extends Component{
     render(){
+        const gradeList = [{type: 'Math', grade: 88}, {type: 'CS', grade: 94}]
         return (
             <div>
                 <img src={defaultUserImage} alt="User"/>
                 <p>Joe Doe</p>
                 <p>joedoe123stonex@mail.com</p>  
+            
+            <center>
+                <LineChart grades={ gradeList } />
+            </center>
             </div>
         );
     };
